8362aa893a2bc9e64d96c9cdc6caa7d5b24bfc8a
Sunil <mk.karbasappa@gmail.com>
2019-06-09 21:11:07 +0530

    added first test cases

--- src/test/java/com/example/demo/SabInfoServicesApplicationTests.java
+++ src/test/java/com/example/demo/SabInfoServicesApplicationTests.java
@@ -1,16 +1,29 @@
 package com.example.demo;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.HomeController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SabInfoServicesApplicationTests {
 
+	@Autowired
+	HomeController homeContr;
+	
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void test() {
+		assertEquals("success",homeContr.test());
+	}
 }
