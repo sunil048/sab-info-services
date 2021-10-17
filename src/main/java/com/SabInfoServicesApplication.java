f290a2e41038f953f0362e6117a928b67068613a
Sunil <mk.karbasappa@gmail.com>
2021-10-17 15:11:09 +0530

    2.0 testing -> changed the code to create war file deployed and tested on tomcat,desbled scurity config

--- src/main/java/com/SabInfoServicesApplication.java
+++ src/main/java/com/SabInfoServicesApplication.java
@@ -4,15 +4,31 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EntityScan(basePackages= {"com.sabtok.persistance.entity","com.sabinfo"})
 @ComponentScan(basePackages= {"com"})
-public class SabInfoServicesApplication {
+public class SabInfoServicesApplication extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SabInfoServicesApplication.class, args);
 	}
+	
+	@RequestMapping("/")
+	public String getStatus() {
+		return "Server is up and running.........";
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SabInfoServicesApplication.class);
+	}
+	
 }
+
+
--- src/main/java/com/sabtok/config/SecurityConfig.java
+++ src/main/java/com/sabtok/config/SecurityConfig.java
@@ -2,11 +2,8 @@ package com.sabtok.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-
+/*
 public class SecurityConfig extends WebSecurityConfigurerAdapter  {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -26,3 +23,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter  {
 				.withUser("admin").password("admin").roles("ROLE_ADMIN");
 	}
 }
+*/
