e89ddcb648f4dad33ece97c9a31930fe1c0938eb
Sunil <mk.karbasappa@gmail.com>
2024-10-20 21:55:58 +0530

    modified code to work in tom cat server

--- src/main/java/com/SabInfoServicesApplication.java
+++ src/main/java/com/SabInfoServicesApplication.java
@@ -2,6 +2,8 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,11 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @EnableJpaRepositories
 @EnableFeignClients
 @EnableScheduling
-public class SabInfoServicesApplication  {
+public class SabInfoServicesApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(SabInfoServicesApplication.class, args);
 	}
-	
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SabInfoServicesApplication.class);
+	}
+
 	@RequestMapping("/status")
 	public String getStatus() {
 		return "Server is up and running.........";
